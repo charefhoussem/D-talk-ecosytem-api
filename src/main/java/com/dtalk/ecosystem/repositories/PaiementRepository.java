a9ff6942c312944456024c29c7e699107a0765ca
hassenmrakbenjebahi <hassen.mrakbenjebahi@esprit.tn>
2024-07-19 18:45:52 +0200

    createPaiement completed

--- src/main/java/com/dtalk/ecosystem/repositories/PaiementRepository.java
+++ src/main/java/com/dtalk/ecosystem/repositories/PaiementRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PaiementRepository extends JpaRepository<Paiement,Long> {
+public interface PaiementRepository extends JpaRepository<Paiement,String> {
 
 }
--- src/main/java/com/dtalk/ecosystem/services/impl/PaiementServiceImpl.java
+++ src/main/java/com/dtalk/ecosystem/services/impl/PaiementServiceImpl.java
@@ -2,7 +2,11 @@ package com.dtalk.ecosystem.services.impl;
 
 import com.dtalk.ecosystem.DTOs.request.paiement.AddPaiementRequest;
 import com.dtalk.ecosystem.DTOs.request.paiement.UpdatePaiementRequest;
+import com.dtalk.ecosystem.entities.Order;
 import com.dtalk.ecosystem.entities.Paiement;
+import com.dtalk.ecosystem.exceptions.ResourceNotFoundException;
+import com.dtalk.ecosystem.repositories.OrderRepository;
+import com.dtalk.ecosystem.repositories.PaiementRepository;
 import com.dtalk.ecosystem.services.PaiementService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,11 +17,20 @@ import java.util.List;
 @RequiredArgsConstructor
 public class PaiementServiceImpl implements PaiementService {
 
+    private final PaiementRepository paiementRepository;
+    private final OrderRepository orderRepository;
+
 
     @Override
     public Paiement createPaiement(AddPaiementRequest request) {
+        Order order = orderRepository.findById(request.getIdOrder()).orElseThrow(()-> new ResourceNotFoundException("order not found " + request.getIdOrder()));
+        if( paiementRepository.findById(request.getRefPaiement()).isPresent()){
+            throw new ResourceNotFoundException("ref paiement already exist");
+        }
 
-        return null;
+        Paiement paiement = Paiement.builder().RefPaiement(request.getRefPaiement()).amount(request.getAmount()).date(request.getDate()).order(order).build();
+
+        return paiementRepository.save(paiement);
     }
 
     @Override
